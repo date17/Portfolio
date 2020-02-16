6eb5c36d6b6a71dcd48d4160721882d89d2f5ef3
date17 <date17@ezweb.ne.jp>
2020-02-16 19:40:50 +0900

    first commit

---
+++ src/components/layout.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Header from "./header";
+import Footer from "./footer";
+import "../static/sass/layout.scss";
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        {this.props.children}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Layout;
